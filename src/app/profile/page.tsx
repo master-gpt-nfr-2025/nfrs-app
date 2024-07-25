578c59617cb58133c645e676819885d9b0de5ce9
Kuba Ratajczyk <kuba.ratajczyk.biz@gmail.com>
2024-07-25 09:37:08 +0200

    Added logout in the profile, bugfix for trash

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,15 +1,42 @@
 "use client";
 import { useUserContext } from "@/components/UserProvider";
-import { Typography } from "@mui/joy";
-import React from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Card, Stack, Typography, Button } from "@mui/joy";
+import React, { useState } from "react";
 
 const Profile = () => {
-	const { user } = useUserContext();
+	const { user, logout } = useUserContext();
+	const [loading, setLoading] = useState(false);
+	const handleLogout = () => {
+		setLoading(true);
+		logout();
+		setLoading(false);
+		window.location.reload();
+	};
 	return (
-		<>
+		<Stack gap={2}>
 			<Typography level="h3">Profil</Typography>
-			<Typography level="h4">{user?.name}</Typography>
-		</>
+			<Card variant="plain">
+				<Stack gap={0}>
+					<Typography level="title-md">Nazwa użytkownika</Typography>
+					<Typography level="h4">{user?.name}</Typography>
+				</Stack>
+
+				<Stack gap={0}>
+					<Typography level="title-md">ID</Typography>
+					<Typography level="h4">{user?.id}</Typography>
+				</Stack>
+				<Button
+					color="primary"
+					onClick={handleLogout}
+					sx={{ maxWidth: 150 }}
+					endDecorator={<Icon icon="ph:sign-out-bold" />}
+					loading={loading}
+				>
+					Wyloguj
+				</Button>
+			</Card>
+		</Stack>
 	);
 };
 
--- src/app/trash/page.tsx
+++ src/app/trash/page.tsx
@@ -3,6 +3,7 @@ import RequirementModel from "@/models/requirement.model";
 import React from "react";
 import connect from "@/config/db";
 import TrashedItems from "@/components/ui/trashed-items";
+import UserModel from "@/models/user.model";
 
 export type TrashedItemType = {
 	_id: string;
@@ -21,6 +22,7 @@ const Trash = async () => {
 	const fetchTrashedItems = async () => {
 		"use server";
 		await connect();
+		await UserModel.findOne();
 		const trashedRequirements = await RequirementModel.find({ trashed: true })
 			.select("_id id name createdAt createdBy trashedAt trashedBy")
 			.populate({
@@ -36,7 +38,6 @@ const Trash = async () => {
 
 	const itemsJSON = JSON.parse(JSON.stringify(items));
 
-	console.log(itemsJSON);
 	return (
 		<Stack direction={"column"} gap={2}>
 			<Typography level="h3">Kosz</Typography>
